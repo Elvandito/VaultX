bf878e763a2c873a45db8b3d9f9b1e31a0c4a479
Elvandito <86403393+Elvandito@users.noreply.github.com>
2026-04-23 08:56:38 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/elvandito/vaultx/MainActivity.kt
+++ android/app/src/main/kotlin/com/elvandito/vaultx/MainActivity.kt
@@ -1,48 +1,6 @@
-plugins {
-    id "com.android.application"
-    id "org.jetbrains.kotlin.android"
-    id "dev.flutter.flutter-gradle-plugin"
-}
+package com.elvandito.vaultx
 
-android {
-    // Namespace harus persis sama dengan package di MainActivity.kt
-    namespace "com.elvandito.vaultx"
-    compileSdk 34
+import io.flutter.embedding.android.FlutterActivity
 
-    defaultConfig {
-        // ApplicationId adalah ID unik aplikasi Anda di sistem Android
-        applicationId "com.elvandito.vaultx"
-        minSdk 21
-        targetSdk 34
-        versionCode 1
-        versionName "1.0.0"
-    }
-
-    buildTypes {
-        release {
-            // Menggunakan debug signing untuk kemudahan build di GitHub Actions tanpa keystore
-            signingConfig signingConfigs.debug
-            minifyEnabled false
-            shrinkResources false
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = '17'
-    }
-}
-
-flutter {
-    // Menunjuk ke folder utama tempat pubspec.yaml berada
-    source '../..'
-}
-
-dependencies {
-    // Dependensi standar Android
-    implementation "org.jetbrains.kotlin:kotlin-stdlib:1.8.22"
+class MainActivity: FlutterActivity() {
 }
